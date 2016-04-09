07ffb71fb6d145c27ffb9a5e98c81ed3c7103a3c
Duane Johnson <duane.johnson@gmail.com>
2016-04-08 21:44:56 -0600

    Create ArtRequest

--- kiosk.py
+++ kiosk.py
@@ -1,13 +1,57 @@
 from Tkinter import *
 from PIL import Image, ImageTk
+from datetime import datetime
 import time
 import cv2
+import os
+import errno   
 
-class mainWindow():
+class ArtRequest():
+
+  def __init__(self, style=None, subject=None):
+    self.style = style
+    self.subject = subject
+    self.uid = self.unique_id()
+
+  def unique_id(self):
+    dt = datetime.now()
+    uid = dt.strftime("%Y-%m-%d.%H-%M-%S")
+    return "{0}.{1}".format(uid, (dt.microsecond / 10000))
+
+  def cwd(self):
+    return os.path.dirname(os.path.realpath(__file__))
+
+  def image_dir(self):
+    return os.path.join(self.cwd(), 'images')
+
+  def request_dir(self):
+    return os.path.join(self.image_dir(), self.uid) + ".request"
+
+  def mkdir_p(self, path):
+    try:
+      os.makedirs(path)
+    except OSError as exc:  # Python >2.5
+      if exc.errno == errno.EEXIST and os.path.isdir(path):
+        pass
+      else:
+        raise
+
+  def save(self):
+    path = self.request_dir()
+    print "Creating dir: ", path
+    self.mkdir_p(path)
+    self.style.save(os.path.join(path, "style.jpg"))
+    self.subject.save(os.path.join(path, "subject.jpg"))
+
+
+class MainWindow():
   times=1
   timestart=time.clock()
   
   def __init__(self):
+    self.last_style_image = None
+    self.last_subject_image = None
+
     self.root = Tk()
     self.root.attributes("-fullscreen", True)
     # self.root.pack(expand=1)
@@ -37,21 +81,18 @@ class mainWindow():
     f1.grid(row=0, column=0)
 
     self.canvas_style = Canvas(f1, width=self.scrw, height=self.scrh, bg="black", highlightthickness=0)
-    # self.canvas_style.create_rectangle(0, 0, self.scrw, self.scrh, fill="black")
     self.canvas_style.pack()
 
     f2 = Frame(self.root)
     f2.grid(row=1, column=0)
 
     self.canvas_subject = Canvas(f2, width=self.scrw, height=self.scrh, bg="black", highlightthickness=0)
-    # self.canvas_subject.create_rectangle(0, 0, self.scrw, self.scrh, fill="black")
     self.canvas_subject.pack()
 
     f3 = Frame(self.root)
     f3.grid(row=0, column=1, rowspan=2, sticky=N+S+E+W)
 
     self.canvas_result = Canvas(f3, width=self.scrw, height=self.scrh, bg="black")
-    # self.canvas_result.create_rectangle(0, 0, self.scrw, self.scrh*2, fill="black")
     self.canvas_result.pack(fill=BOTH, expand=1)
 
     self.root.after(0, self.start)
@@ -71,16 +112,24 @@ class mainWindow():
     elif key == "space":
       # Toggle capture window between style and subject
       if self.capture_target == "style":
+        self.last_style_image = self.image
+        self.last_style_photo = self.photo
         self.capture_target = "subject"
       elif self.capture_target == "subject":
+        self.last_subject_image = self.image
+        self.last_subject_photo = self.photo
         self.capture_target = "result"
+        self.make_art_request()
       elif self.capture_target == "result":
         self.capture_target = "style"
+
+      # Whenever state changes, "freeze" the last frame
+      self.show_last_images()
 
   def capture_to_image(self, frame):
     """Convert an OpenCV camera frame to a PhotoImage"""
     cv2img = cv2.cvtColor(cv2.flip(frame, 1), cv2.COLOR_BGR2RGBA)
-    return ImageTk.PhotoImage(Image.fromarray(cv2img))
+    return Image.fromarray(cv2img)
 
   def choose_capture_canvas(self):
     if self.capture_target == "style":
@@ -92,22 +141,38 @@ class mainWindow():
     else:
       raise Exception("Capture Target in unknown state: {0}".format(self.capture_target))
 
+  def show_last_images(self):
+    if (self.last_style_image is not None and self.capture_target != "style"):
+      self.canvas_style.delete(ALL)
+      self.canvas_style.create_image(self.margin_left, self.margin_top, image=self.last_style_photo, anchor=NW)
+    
+    if (self.last_subject_image is not None and self.capture_target != "subject"):
+      self.canvas_subject.delete(ALL)
+      self.canvas_subject.create_image(self.margin_left, self.margin_top, image=self.last_subject_photo, anchor=NW)
+
+  def make_art_request(self):
+    req = ArtRequest(style=self.last_style_image, subject=self.last_subject_image)
+    print "ArtRequest ID: ", req.uid
+    req.save()
+
   def start(self):
     err, frame = self.capture.read()
     if frame is not None:
-      self.im = self.capture_to_image(frame)
+      self.image = self.capture_to_image(frame)
+      self.photo = ImageTk.PhotoImage(self.image)
 
       canvas = self.choose_capture_canvas()
       canvas.delete(ALL)
-      canvas.create_image(self.margin_left, self.margin_top, image=self.im, anchor=NW)
+      canvas.create_image(self.margin_left, self.margin_top, image=self.photo, anchor=NW)
       self.root.update()
 
       self.times += 1
       if self.times%33==0:
-        print "%.02f FPS"%(self.times/(time.clock()-self.timestart))
+        fps = self.times / (time.clock() - self.timestart)
+        print "{0}: {1} FPS".format(time.clock(), fps)
 
       # Schedule the next camera frame grab
       self.root.after(10, self.start)
 
 if __name__ == '__main__':
-  mainWindow()
+  MainWindow()
