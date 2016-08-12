364d047c6e72686ad3c6c27cc7f48f314203d037
Alex Boyd <aboyd@instructure.com>
2016-08-11 22:47:11 -0600

    how about an initial request?

--- printer_assets/stuff.js
+++ printer_assets/stuff.js
@@ -2,10 +2,27 @@
 console.log('yo');
 
 Image = React.createClass({
+  getInitialState: function() {
+    return {
+      status: {
+        images: []
+      }
+    };
+  },
+
+  componentDidMount: function() {
+    $.ajax('/status', {
+      success: (data) => {
+        this.setState({status: data});
+      }
+    })
+  },
+
   render: function() {
-    return <div>weeee!</div>;
+    return <div>weeee! status is {JSON.stringify(this.state.status)}</div>;
   }
 });
+
 
 
 ReactDOM.render(<Image/>, document.getElementById('stuff'));
