a519c6254acc7203711729f27ab5aa0ec3792648
Alex Boyd <aboyd@instructure.com>
2016-08-12 00:26:27 -0600

    poll status every 10 seconds

--- printer_assets/stuff.js
+++ printer_assets/stuff.js
@@ -9,11 +9,42 @@ var ImageGrid = React.createClass({
   },
 
   componentDidMount: function() {
-    $.ajax('/status', {
+    this.startRequest();
+  },
+
+  startRequest: function() {
+    if (this.noMoreRequests) {
+      return;
+    }
+
+    console.log('starting status request');
+
+    this.currentRequest = $.ajax('/status', {
       success: (data) => {
+        console.log('status request succeeded.');
+        this.currentRequest = null;
         this.setState({status: data});
+        this.startRequestLater();
+      },
+      error: () => {
+        console.log('status request failed.');
+        this.currentRequest = null;
+        this.startRequestLater();
       }
     });
+  },
+
+  startRequestLater: function() {
+    if (this.noMoreRequests) {
+      return;
+    }
+
+    console.log('scheduling status request in 10 seconds');
+
+    this.curentTimeout = setTimeout(() => {
+      this.currentTimeout = null;
+      this.startRequest();
+    }, 10000);
   },
 
   render: function() {
@@ -24,6 +55,15 @@ var ImageGrid = React.createClass({
         })
       }</div>
     </div>;
+  },
+
+  componentWillUnmount = function() {
+    this.noMoreRequests = true;
+    if (this.currentTimeout != null) {
+      clearTimeout(this.currentTimeout);
+    } else if (this.currentRequest != null) {
+      this.currentRequest.abort();
+    }
   }
 });
 
