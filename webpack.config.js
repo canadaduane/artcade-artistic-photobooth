ca825ff909e2b1338c80df095da0d09d3362d199
Alex Boyd <alex@opengroove.org>
2017-06-18 22:50:06 -0600

    webpack all the things

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'printer_assets/stuff.jsx'),
+  output: {
+    path: path.resolve(__dirname, 'printer_assets/generated'),
+    filename: 'bundle.js',
+    libraryTarget: 'var',
+    library: 'Arts'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2016', 'stage-2', 'react'],
+        }
+      }
+    ]
+  }
+};
